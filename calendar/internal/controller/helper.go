93a7475c51e90b519cb4b61facf0b1354376d25e
achi3v3 <amir-tutaev@mail.ru>
2026-03-27 14:12:49 +0500

    fix(back): fix errors with linter

--- calendar/internal/controller/helper.go
+++ calendar/internal/controller/helper.go
@@ -21,7 +21,9 @@ func writeJSON(w http.ResponseWriter, data interface{}) {
 func writeError(w http.ResponseWriter, msg string, code int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(map[string]string{"error": msg})
+	if err := json.NewEncoder(w).Encode(map[string]string{"error": msg}); err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+	}
 }
 
 func checkMethod(w http.ResponseWriter, r *http.Request, method string) bool {
--- calendar/internal/controller/type.go
+++ calendar/internal/controller/type.go
@@ -1,3 +1,4 @@
+// Package controller обрабатывает HTTP-запросы и делегирует бизнес-логику слою usecase
 package controller
 
 import (
@@ -6,11 +7,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Handler обрабатывает HTTP-запросы к эндпоинтам календаря
 type Handler struct {
 	service *usecase.Service
 	logger  *logrus.Logger
 }
 
+// New создаёт и инициализирует новый экземпляр Handler
 func New(service *usecase.Service, logger *logrus.Logger) *Handler {
 	return &Handler{
 		service: service,
